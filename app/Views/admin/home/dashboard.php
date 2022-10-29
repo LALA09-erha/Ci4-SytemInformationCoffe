cd0d2efacbb37a457e1db3a352c1b46e8ce4daec
erha <fikri41153.pesonaxii@gmail.com>
2022-10-29 17:04:10 +0700

    update

--- app/Views/admin/home/dashboard.php
+++ app/Views/admin/home/dashboard.php
@@ -17,7 +17,13 @@
 <div class="row">
 
     <!-- JUMLAH MENU -->
-    <div class="col-lg-4 col-md-6 mb-4">
+    <div class="col-lg-<?php
+    if(session()->get('idadmin')){
+        echo '4';
+    }else{
+        echo '6';
+    }
+    ?> col-md-6 mb-4">
         <div class="card border-left-danger shadow h-100 py-2">
             <div class="card-body">
                 <div class="row no-gutters align-items-center">
@@ -42,7 +48,13 @@
     </div>
 
     <!-- JUMLAH SEMUA REVIEW -->
-    <div class="col-lg-4 col-md-6 mb-4">
+    <div class="col-lg-<?php
+    if(session()->get('idadmin')){
+        echo '4';
+    }else{
+        echo '6';
+    }
+    ?> col-md-6 mb-4">
         <div class="card border-left-success shadow h-100 py-2">
             <div class="card-body">
                 <div class="row no-gutters align-items-center">
@@ -65,6 +77,7 @@
         </div>
     </div>
 
+    <?php if(session()->get('idadmin')): ?>
     <!-- JUMLAH KEDAI -->
     <div class="col-lg-4 col-md-6 mb-4">
         <div class="card border-left-success shadow h-100 py-2">
@@ -88,6 +101,7 @@
             </div>
         </div>
     </div>
+    <?php endif; ?>
     
 </div>
 
